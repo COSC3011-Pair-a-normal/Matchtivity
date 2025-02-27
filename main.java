ff6667f94d8012abc5f1ceb6749b752d502b753f
cniemitalo <159483630+cniemitalo@users.noreply.github.com>
2025-02-27 13:22:17 -0700

    added startNew and startSaved

--- main.java
+++ main.java
@@ -1,22 +1,48 @@
 import javax.swing.*;
+import java.awt.*; 
+import java.awt.event.*; 
 
-public class Main {
+public class Main implements ActionListener{
+    //create buttons 
+    private JButton startNew; 
+    private JButton startSaved;
+
     public static void main(String[] args) {
-        // Create a new JFrame (window)
-        JFrame frame = new JFrame("Pair-a-normal Matchtivity");
+        Main gui = new Main(); 
+        gui.go(); 
+    }
 
-        // Set the default close operation so the program will exit when the window is closed
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public void go() {
+        //create new frame and page title
+        JFrame frame = new JFrame("Pair-A-Normal Matchtivity"); 
 
         JLabel label = new JLabel("[under construction]", SwingConstants.CENTER);
+        label.setFont(new Font("Serif", Font.PLAIN, 60)); 
+        label.setOpaque(true); 
+        label.setBackground(Color.white); 
 
-        // Add the label to the frame
-        frame.add(label);
+        JPanel buttonPanel = new JPanel(); 
+        buttonPanel.setBackground(Color.white); 
 
-        // Set the size of the window
-        frame.setSize(1000, 1000);
+        startNew = new JButton("Start New Game"); 
+        startSaved = new JButton("Start Saved Game"); 
 
-        // Make the window visible
-        frame.setVisible(true);
+        //for clicking the button 
+        startNew.addActionListener(this); 
+
+        //add buttons to panel 
+        buttonPanel.add(startNew);
+        buttonPanel.add(startSaved);  
+
+        frame.add(label, BorderLayout.NORTH); 
+        frame.add(buttonPanel, BorderLayout.CENTER);  
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        frame.setSize(200,200); 
+        frame.setVisible(true); 
+    }
+
+    public void actionPerformed(ActionEvent event) { 
+        //change to connect with choose difficulty
+        startNew.setText("I've been clicked!"); 
     }
 }
