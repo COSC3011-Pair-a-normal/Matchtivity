ec5a59de52199d51cc9ac9353dcc504a6935aa95
Caden Mcfate <113316495+cadenmcfate@users.noreply.github.com>
2025-03-27 12:21:34 -0600

    Update GameController.java, added card flipping functionality

--- src/main/java/com/GameController.java
+++ src/main/java/com/GameController.java
@@ -23,12 +23,35 @@ public class GameController implements Initializable {
         System.out.println("controller is working");
     }
 
+    public static final Image backImage = new Image("/images/back_of_card.png");
+
     private void initializeImageView() {
         for (int i = 0; i < imagesFlowPane.getChildren().size();i++) {
             ImageView imageView = (ImageView) imagesFlowPane.getChildren().get(i);
-            Image image = new Image("/images/classic/2_of_clubs.png");
+            //Image image = new Image("/images/classic/2_of_clubs.png");
+            Image image = backImage;
             imageView.setImage(image);
+
+            imageView.setUserData(i); // Each card has an identification number 0,1,...,n
+
+            // functionality for making the folder for the current card images will go here
+
+            imageView.setOnMouseClicked(event -> {
+                flipCard((int) imageView.getUserData());
+            });
         }
+    }
+
+    private void flipCard(int cardID) {
+        ImageView imageView = (ImageView) imagesFlowPane.getChildren().get(cardID);
+        Image image;
+        System.out.println(imageView.getImage() == backImage);
+        if (imageView.getImage() == backImage) {
+            image = new Image("/images/classic/ace_of_spades.png");
+        } else {
+            image = backImage;
+        }
+        imageView.setImage(image);
     }
 
 }
